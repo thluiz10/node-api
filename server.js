b27e88ef8466e1b42d3e6bb2aee4261b70725998
Lucas Domingues <lucasdomi@globo.com>
2019-05-17 02:05:46 -0300

    add: creates connection of the mongoose with mongo

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const mongoose = require ('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true})
 // req contains all possible details of the request and res is response to the request
 app.get('/', (req,res) => {
   res.send("Hello World")
