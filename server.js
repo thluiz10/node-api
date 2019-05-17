c6d7db6dc67783e8d54e45b81ac1bcca33d75b61
Lucas Domingues <lucasdomi@globo.com>
2019-05-17 01:29:17 -0300

    add: create first route

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const app = express();
 
+// req contains all possible details of the request and res is response to the request
+app.get('/', (req,res) => {
+  res.send("Hello World")
+});
+
 app.listen(3001);
