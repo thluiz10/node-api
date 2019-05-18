db0fa272530d7285bf353ee8f67e847748bf09f0
Lucas Domingues <lucasdomi@globo.com>
2019-05-18 12:11:46 -0300

    add: create controller for delete one product

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -20,9 +20,12 @@ module.exports = {
 
   async update (req, res) {
     const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new : true});
-
     return res.json(product)
   },
 
-  async destroy () {}
+  async destroy (req,res) {
+    await Product.findByIdAndRemove(req.params.id);
+
+    return res.send();
+  }
 }
