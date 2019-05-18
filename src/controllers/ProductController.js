25cf1c884b256913001c14095fc30744e090d342
Lucas Domingues <lucasdomi@globo.com>
2019-05-18 12:33:46 -0300

    refact: :lipstick:

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -5,13 +5,13 @@ const Product = mongoose.model ('Product');
 module.exports = {
   async index(req, res) {
     const { page = 1 } = req.query;
-    const products = await Product.paginate( { }, { page, limit: 10});
+    const products = await Product.paginate({}, { page, limit: 10});
     return res.json (products);
   },
 
   async show (req,res) {
-    const product = await Product.findById(req.params.id)
-    return res.json(product)
+    const product = await Product.findById(req.params.id);
+    return res.json(product);
   },
 
   async store (req, res) {
@@ -21,7 +21,7 @@ module.exports = {
 
   async update (req, res) {
     const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new : true});
-    return res.json(product)
+    return res.json(product);
   },
 
   async destroy (req,res) {
