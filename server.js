5bff73da4950b0a83b2840b22a19c306947a1826
Lucas Domingues <lucasdomi@globo.com>
2019-05-18 01:07:30 -0300

    add: create first api model for products

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const mongoose = require ('mongoose');
+const requireDir = require('require-dir');
 
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true})
+requireDir('./src/models')
+
+// const Product = mongoose.model('Product');
+
 // req contains all possible details of the request and res is response to the request
 app.get('/', (req,res) => {
-  res.send("Hello World")
+  // Product.create({ 
+  //   title: "React native", 
+  //   description: "Build native apps with React",
+  //   url:"http://github.com/facebook/react-native"
+  // })
+  return res.send("Hello World")
 });
 
 app.listen(3001);
