bc8b2fdffbce3eddc2e294b0140a957aa1fb0d3c
Lucas Domingues <lucasdomi@globo.com>
2019-05-19 13:00:23 -0300

    refact: :lipstick:

--- src/tests/test.js
+++ src/tests/test.js
@@ -11,35 +11,35 @@ describe('Products', () => {
 
   // No describe podemos passar um texto para identificação 
   describe('/GET Products', () => {
-        it('Testing GET from api', (done) => {
-            chai.request(server) // Endereço do servidor
-                .get('/api/products') // endpoint que vamos testar
-                .end((err, res) => { // testes a serem realizados
-                    res.should.have.status(200); // verificando se o retorno e um status code 200
-                    res.body.should.be.a('Object'); // Verificando se o retorno e um array
-                    res.body.should.all.have.property('docs');
-                    res.body.should.all.have.property('total');
-                    res.body.should.all.have.property('page');
-                    res.body.should.all.have.property('pages');
-                  done();
+    it('Testing GET from api', (done) => {
+        chai.request(server) // Endereço do servidor
+            .get('/api/products') // endpoint que vamos testar
+            .end((err, res) => { // testes a serem realizados
+              res.should.have.status(200); // verificando se o retorno e um status code 200
+              res.body.should.be.a('Object'); // Verificando se o retorno e um array
+              res.body.should.all.have.property('docs');
+              res.body.should.all.have.property('total');
+              res.body.should.all.have.property('page');
+              res.body.should.all.have.property('pages');
+              done();
             });
-        });
+    });
   });
   
   describe('/POST Product', () => {
     it('Create product', (done) => {
-        let product = {
-            title: "Test", 
-            description: "Test post",
-            url: "test.com"
-        }
-        chai.request(server)
-        .post('/api/products')
-        .send(product) // send file
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
+      let product = {
+          title: "Test", 
+          description: "Test post",
+          url: "test.com"
+      }
+      chai.request(server)
+      .post('/api/products')
+      .send(product) // send file
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
     });
   }); 
 
