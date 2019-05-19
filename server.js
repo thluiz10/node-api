5b4dd2139b30747bd00e6cdb33c18bd8bc77d0f5
Lucas Domingues <lucasdomi@globo.com>
2019-05-19 01:11:24 -0300

    add: enable cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require ('mongoose');
+const cors = requre('cors');
 const requireDir = require('require-dir');
 
 const app = express();
 app.use(express.json()); //allow me to send json to the application
+app.use(cors()); //enable access for any doms
 
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true, useFindAndModify: false})
 requireDir('./src/models')
