a6c805b7443beee33be4a7ac4d405d95246d5553
TH <thluiz10@gmail.com>
2020-03-16 17:58:53 -0300

    Updating server.js and jenkins-compose.yml

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json()); //allow me to send json to the application
 app.use(cors()); //enable access for any doms
 
 mongoose.connect(
-  'mongodb://mongo:27017/nodeapi', {
+  'mongodb://db:27017/nodeapi', {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: false
