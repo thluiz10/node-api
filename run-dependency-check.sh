7beee50e0266873076560a20f9746c5ea4f47d91
TH <thluiz10@gmail.com>
2020-03-16 16:41:06 -0300

    Updating dependency-check path

--- run-dependency-check.sh
+++ run-dependency-check.sh
@@ -1,1 +1,1 @@
-/home/th/Desktop/node-api/dependency-check/bin/dependency-check.sh --project node-api --scan src/**/*.js --scan node_modules/ --scan *.json --exclude src/tests/ --format HTML --format JSON --out reports
+$PWD/dependency-check.sh --project node-api --scan src/**/*.js --scan node_modules/ --scan *.json --exclude src/tests/ --format HTML --format JSON --out reports
